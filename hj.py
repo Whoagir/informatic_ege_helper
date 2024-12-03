74cf1eb6a8785df304a61a0142aa4ee41c16aaea
whoagi <whoagir@yandex.ru>
2024-12-03 16:47:27 +0300

    create README.md

--- hj.py
+++
@@ -1,4 +0,0 @@
-n=646
-s=bin(n)[2:]
-s1=s.rfind('0')
-print(s1)
--- nb.py
+++
@@ -1,7 +0,0 @@
-def ctb(number,base):
-    s=''
-    while number>0:
-        s=s+str(number%base)
-        number=number//base
-    return s[::-1]
-print(ctb(int(input()),int(input())))
